02b57484b4240f77ad624e768209a11bf692fca7
Brehyma Traore <brehymatraore50@gmail.com>
2024-08-06 22:05:22 -0700

    A new view for State objects that handles all default RESTFul API actions

---
+++ api/v1/views/states.py
@@ -0,0 +1,73 @@
+#!/usr/bin/python3
+"""a new view for State objects that handles all default RESTFul API actions"""
+
+
+from flask import Flask, jsonify, abort, request
+from models import storage
+from models.state import State
+from api.v1.views import app_views
+
+
+@app_views.route("/states", methods=['GET'], strict_slashes=False)
+def get_states():
+    """Retrieves the list of all State objects"""
+    states = storage.all(State).values()
+    return jsonify([state.to_dict() for state in states])
+
+
+@app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
+def get_state(state_id):
+    """Retrieves a State object"""
+    state = storage.get(State, state_id)
+
+    if not state:
+        abort(404)
+
+    return jsonify(state.to_dict())
+
+
+@app_views.route('states/<state_id>', methods=['DELETE'], strict_slashes=False)
+def delete_state(state_id):
+    """Deletes a State object"""
+    to_delete = storage.get(State, state_id)
+    if not to_delete:
+        abort(404)
+
+    return {}, 200
+
+
+@app_views.route('/states', methods=['POST'], strict_slashes=False)
+def create_state():
+    """Create a state"""
+    new_state = request.get_json()
+    if not new_state:
+        abort(400, "Not a JSON")
+
+    if 'name' not in new_state:
+        abort(400, description="Missing name")
+
+    state = State(**new_state)
+    storage.new(state)
+    storage.save()
+
+    return jsonify(state.to_dict()), 201
+
+
+@app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """updates a State object"""
+    state = storage.get(State, state_id)
+    if not state:
+        abort(404)
+
+    new_data = request.get_json()
+    if not new_data:
+        abort(400, description="Not a JSON")
+
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    for key, value in new_data.items():
+        if key not in ignore_keys:
+            setattr(state, key, value)
+
+    storage.save()
+    return jsonify(state.to_dict()), 200
